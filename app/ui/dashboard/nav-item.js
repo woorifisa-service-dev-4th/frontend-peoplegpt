50e079ea1ec383debab01efc263bff85a9936f71
박찬진 <68864422+developerChans@users.noreply.github.com>
2025-01-23 17:48:21 +0900

    Revert "Fix: css style 수정"

--- app/ui/dashboard/nav-item.js
+++ app/ui/dashboard/nav-item.js
@@ -6,7 +6,7 @@ export default function NavItem({ href, icon, label, active, onClick }) {
     return (
         <Link href={href}>
             <div
-                className={`bg-[#E1E1E1] h-20 rounded-2xl flex items-center justify-center cursor-pointer px-10 ${
+                className={`bg-[#F4F4F4] h-20 rounded-2xl flex items-center justify-center cursor-pointer px-10 ${
                     active ? "text-[#0067AC]" : "text-black"
                 }`}
                 onClick={onClick}
