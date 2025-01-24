f5e1076001f86a367c4d7ea6f2a5c7249a931443
noeyoes <>
2025-01-24 17:15:12 +0900

    Feat: 채팅 입력란 제목 필드 구현

--- app/ui/chat.js
+++ app/ui/chat.js
@@ -1,31 +1,44 @@
-'use client';
-import { useState } from 'react';
-import { tagsData } from '../lib/data';
+"use client";
+import { useState } from "react";
+import { tagsData } from "../lib/data";
 
-export default function Chat({onSubmit, isComment, activeMenu, setActiveMenu}) {
+export default function Chat({
+    onSubmit,
+    isComment,
+    activeMenu,
+    setActiveMenu,
+}) {
     const [isOpen, setIsOpen] = useState(false);
+    const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
 
     const [currentTag, setCurrentTag] = useState("");
     const isQnA = activeMenu === "qna";
 
+    // TODO: title 저장 로직 추가
     const handleSubmit = () => {
         if (content.trim() !== "") {
             onSubmit(content);
             setContent("");
         }
-    };  
+    };
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
         console.log(isOpen);
-    }
+    };
     return (
         <div className="h-1/5 flex flex-col ">
             {/* 메시지 입력 영역 */}
             <div className="flex-1 px-2">
                 <textarea
-                    className="w-full h-full resize-none p-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="w-full h-1/4 resize-none pl-4 pt-1 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder="Type title here"
+                    value={title}
+                    // TODO: onChange 추가
+                ></textarea>
+                <textarea
+                    className="w-full h-4/6 resize-none p-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     placeholder="Type your message here"
                     value={content}
                     onChange={(e) => setContent(e.target.value)}
@@ -33,19 +46,23 @@ export default function Chat({onSubmit, isComment, activeMenu, setActiveMenu}) {
             </div>
 
             {/* 하단 영역 */}
-            <div className={`flex items-center ${isComment ? 'justify-end' : 'justify-between'} p-4 bg-white`}>
+            <div
+                className={`flex items-center ${
+                    isComment ? "justify-end" : "justify-between"
+                } p-4 bg-white`}
+            >
                 {/* 해시태그 버튼 */}
 
                 <div className="flex gap-2">
-                    {(!isComment && isQnA) && 
+                    {!isComment && isQnA && (
                         <button
                             onClick={toggleDropdown}
                             className="flex items-center justify-center w-10 h-10 border rounded-lg shadow-sm bg-gray-100 hover:bg-gray-200 focus:outline-none"
                         >
                             #
                         </button>
-                    }
-                
+                    )}
+
                     {/* 태그 선택 */}
                     {isOpen && (
                         <div className="flex flex-row mb-2 w-50 bg-white border border-gray-300 rounded-lg shadow-lg">
@@ -64,7 +81,7 @@ export default function Chat({onSubmit, isComment, activeMenu, setActiveMenu}) {
                         </div>
                     )}
                     {/* 선택된 태그 */}
-                    {(currentTag && !isOpen) && (
+                    {currentTag && !isOpen && (
                         <div className="flex items-center gap-2">
                             <p className="px-4 py-2 text-sm text-gray-700 bg-gray-100 rounded-lg">
                                 {currentTag}
@@ -72,17 +89,15 @@ export default function Chat({onSubmit, isComment, activeMenu, setActiveMenu}) {
                         </div>
                     )}
                 </div>
-                
 
                 {/* 전송 버튼 */}
-                <button 
-                  className="px-6 py-3 text-white bg-blue-500 rounded-lg shadow-sm hover:bg-blue-600"
-                  onClick={handleSubmit}
+                <button
+                    className="px-6 py-3 text-white bg-blue-500 rounded-lg shadow-sm hover:bg-blue-600"
+                    onClick={handleSubmit}
                 >
                     Send
                 </button>
             </div>
-
         </div>
     );
 }
